c9c24befc81920ac759cbc763c4cccba0b65e023
Sivana Alexa Hamer <sivanahamer@gmail.com>
2022-06-18 11:48:50 -0600

    VIS: Added tags to points

--- 3-data_visualization/src/visualization.py
+++ 3-data_visualization/src/visualization.py
@@ -1,5 +1,5 @@
 from pandas import read_csv
-from matplotlib.pyplot import scatter, show, title, ylabel, xlabel, legend
+from matplotlib.pyplot import scatter, show, title, ylabel, xlabel, plot, text
 
 #Carga el archivo
 df = read_csv("./data/books.csv", error_bad_lines=False)
@@ -10,5 +10,20 @@ resennas_cantidad = df["text_reviews_count"]
 title("Cantidad de reseñas comparado a la clasificación promedio")
 xlabel("Cantidad de reseñas")
 ylabel("Clasificación promedio")
+scatter(resennas_cantidad, calificacion_promedio)
+show()
+
+#Generamos un grafico de calificaciones y reseñas, marcando los puntos mas grandes de reseñas
+calificacion_promedio = df["average_rating"]
+resennas_cantidad = df["text_reviews_count"]
+title("Cantidad de reseñas comparado a la clasificación promedio")
+xlabel("Cantidad de reseñas")
+ylabel("Clasificación promedio")
+for index, libro in df.iterrows():
+    y = libro["average_rating"]
+    x = libro["text_reviews_count"]
+    plot(x,y, 'bo')
+    if libro["text_reviews_count"] > 40000:
+        text(x, y * (1 - 0.05), libro['title'], fontsize=12)
 scatter(resennas_cantidad, calificacion_promedio)
 show()
