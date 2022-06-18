41761f0c6f0c7dc048e8e223dafb28dd83d8a132
Sivana Alexa Hamer <sivanahamer@gmail.com>
2022-06-18 11:40:14 -0600

    VIS: Added title and labels to graphs

--- 3-data_visualization/src/visualization.py
+++ 3-data_visualization/src/visualization.py
@@ -1,10 +1,14 @@
 from pandas import read_csv
-from matplotlib.pyplot import scatter, show
+from matplotlib.pyplot import scatter, show, title, ylabel, xlabel, legend
 
 #Carga el archivo
 df = read_csv("./data/books.csv", error_bad_lines=False)
 
+#Generamos un grafico de calificaciones y reseñas
 calificacion_promedio = df["average_rating"]
 resennas_cantidad = df["text_reviews_count"]
-scatter(calificacion_promedio, resennas_cantidad)
+title("Cantidad de reseñas comparado a la clasificación promedio")
+xlabel("Cantidad de reseñas")
+ylabel("Clasificación promedio")
+scatter(resennas_cantidad, calificacion_promedio)
 show()
