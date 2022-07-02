a0a0788a597b6aa1b989ce3585666b1fcb2ad126
Sivana Alexa Hamer <sivanahamer@gmail.com>
2022-07-02 08:20:06 -0600

    Solution processing de mas populares

--- 2-data_processing/src/solutions.py
+++ 2-data_processing/src/solutions.py
@@ -1,6 +1,13 @@
 from pandas import read_csv
 
-#Leer archivo
+# Lea el archivo de .\data\spotify.csv.
 df = read_csv("./data/spotify.csv")
 
-print(df)
+# Obtenga las cien canciones más populares (las más cercanas a 1).
+print(df["popularity"])
+
+df_ordenado = df.sort_values(by="popularity", ascending=False)
+print(df_ordenado["popularity"])
+mas_populares = df_ordenado.head(100)
+
+mas_populares.to_csv("./data/mas_populares.csv", index=False)
